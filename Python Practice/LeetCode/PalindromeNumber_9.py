09af3013df938524f8bf221e61e6b36c815fd68a
VincentCHEN96 <vincentchen96@foxmail.com>
2019-09-21 17:40:43 +0800

    删除了误传的文件夹

--- Python Practice/LeetCode/PalindromeNumber_9.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        number = str(x)
-        for i in range(0, int(len(number) / 2 + 1)):
-            if number[i] != number[-(i+1)]:
-                return False
-
-        return True
--- Python Practice/LeetCode/SearchInsertPosition_35.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def searchInsert(self, nums: [int], target: int) -> int:
-        i = 0
-        while i <= len(nums):
-            if i < len(nums) and nums[i] < target:
-                i += 1
-            else:
-                return i
